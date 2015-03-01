75257e108a30cd8b3df973eef83f0320bb1e6645
ComFreek <comfreek@outlook.com>
2015-03-01 17:16:15 +0100

    Removed single IP delay test
    irrelevant wrt. the current bug #12

--- test/components/delay.js
+++ test/components/delay.js
@@ -2,34 +2,7 @@
 
 var fbp = require('../..');
 
-describe('delay', function() {
-  it('should exactly delay a single IP', function(done) {
-    var DELAY = 1000;
-    var DELAY_MAX_DIFF = 150;
-    this.timeout(DELAY + DELAY_MAX_DIFF);
-    
-    var network = new fbp.Network();
-    
-    var result = [];
-
-    var sender   = network.defProc(MockSender([42]));
-    var delay    = network.defProc('./components/delay.js');
-    var receiver = network.defProc(MockReceiver(result));
-
-    network.initialize(delay, 'INTVL', DELAY);
-    network.connect(sender, 'OUT', delay, 'IN', 5);
-    network.connect(delay, 'OUT', receiver, 'IN');
-    
-    var startTime = Date.now();
-    
-    network.run(new fbp.FiberRuntime(), { trace: false }, function () {
-      expect(result).to.deep.equal([42]);
-      var diffTime = Date.now() - startTime;
-      expect(Math.abs(diffTime - DELAY)).to.be.below(DELAY_MAX_DIFF);
-      done();
-    });
-  });
-  
+describe('delay', function() { 
   it('should exactly delay multiple IPs', function(done) {
     var DELAY = 1000;
     var DELAY_MAX_DIFF = 300;
